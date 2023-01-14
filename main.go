b811109f6058f49589e66e704fce82b09e8aacc1
Ttibsi <ashisbitt@icloud.com>
2023-01-14 15:38:24 +0000

    step 1 - read from a database and display names

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import "github.com/Ttibsi/web-server-test/pkg"
+import "fmt"
 
 func main() {
+	fmt.Println("marker")
 	pkg.Init()
 }
--- pkg/server.go
+++ pkg/server.go
@@ -1,5 +1,41 @@
 package pkg
 
-func Init () int {
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func read_from_db() []string {
+	db, err := sql.Open("sqlite3", "./db.db")
+	handle_error(err)
+	rows, err := db.Query("SELECT user_name FROM users;")
+	handle_error(err)
+	defer rows.Close()
+
+	var users []string 
+
+	for rows.Next() {
+		var u string
+		err := rows.Scan(&u)
+		handle_error(err)
+		users = append(users, u)
+	}
+	
+	return users
+}
+
+func handle_error(e error) {
+	if e != nil {
+		log.Panicln(e.Error())
+	}
+}
+
+func Init() int {
+	fmt.Println("marker")
+	users := read_from_db()
+	fmt.Println(users)
 	return 0
 }
