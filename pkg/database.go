3fcdf17d011bb9cfea9ceb67851b5094ee58a4bc
Ttibsi <ashisbitt@icloud.com>
2023-01-14 21:06:28 +0000

    Convert file name

--- pkg/database.go
+++ pkg/database.go
@@ -2,17 +2,22 @@ package pkg
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func read_from_db() []string {
+func Handle_error(e error) {
+	if e != nil {
+		log.Panicln(e.Error())
+	}
+}
+
+func Read_from_db() []string {
 	db, err := sql.Open("sqlite3", "./db.db")
-	handle_error(err)
+	Handle_error(err)
 	rows, err := db.Query("SELECT user_name FROM users;")
-	handle_error(err)
+	Handle_error(err)
 	defer rows.Close()
 
 	var users []string 
@@ -20,22 +25,10 @@ func read_from_db() []string {
 	for rows.Next() {
 		var u string
 		err := rows.Scan(&u)
-		handle_error(err)
+		Handle_error(err)
 		users = append(users, u)
 	}
 	
 	return users
 }
 
-func handle_error(e error) {
-	if e != nil {
-		log.Panicln(e.Error())
-	}
-}
-
-func Init() int {
-	fmt.Println("marker")
-	users := read_from_db()
-	fmt.Println(users)
-	return 0
-}
